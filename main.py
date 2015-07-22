6ea7d30548fb528a7fed0794485dd77470298c9a
MinestoPix <zan.stopar@gmail.com>
2015-07-22 14:11:50 +0200

    Add wall perspective

--- main.py
+++ main.py
@@ -40,12 +40,24 @@ camX = camY = 0
 
 zoom = 0
 
-scale = 128
+scale = 32
 
 
 wood_floor = pygame.image.load("wood_floor.png")
 wood_floor = pygame.transform.rotate(wood_floor, 45)
 wood_floor = pygame.transform.smoothscale(wood_floor, (int(scale) + 1, int(scale * 0.5 + 1)))
+
+
+wall_ang = math.atan2(1, 2)
+to_deg = (180/math.pi)
+
+
+brick_wall = pygame.image.load("brick_wall2.png")
+# brick_wall = pygame.transform.rotate(brick_wall, - wall_ang * to_deg)
+# bww, bwh = brick_wall.get_size()
+# brick_wall = brick_wall.subsurface([[math.sin(wall_ang) * 64, 0], [bww - (math.sin(wall_ang) * 64 * 2), bwh]])
+# brick_wall = brick_wall.subsurface([[28, 0], [math.sqrt(2) * 64 - 50, 80]])
+
 
 def render():
 
@@ -60,6 +72,11 @@ def render():
     for x in range(20):
         for y in range(20):
             gameDisplay.blit(wood_floor, (WIDTH/2 + x*scale/2 - y*scale/2, HEIGHT/4 + x*scale/4 + y*scale/4))
+            # points = [[200 + 50 * x, 50 + 20 * x], [250 + 50 * x, 70 + 20 * x], [200 + 50 * x, 90 + 20 * x], [150 + 50 * x, 70 + 20 * x]]
+            # pygame.draw.lines(gameDisplay, black, True, points)
+            
+
+    gameDisplay.blit(brick_wall, (WIDTH/2, HEIGHT/4 + 4))
 
     pygame.display.flip()
 
